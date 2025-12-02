f92bb75606b42049ce6d71c9ecb97d7f90f565c8
Artur <72407422+Sevn9@users.noreply.github.com>
2025-12-03 00:35:21 +0300

    added gateway to docker

--- gateway/internal/handler/auth.go
+++ gateway/internal/handler/auth.go
@@ -8,8 +8,8 @@ import (
 )
 
 type registerRequest struct {
-	Username string `form:"username" binding:"required"`
-	Password string `form:"password" binding:"required"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 type loginRequest struct {
